2483616d413dd3cae4362eda06fe45602a8ed0df
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-06 22:23:32 +0530

    temp

--- controllers/url.js
+++ controllers/url.js
@@ -1,3 +1,19 @@
-async function generateShortURL(request,response){
-    
+const {nanoid} = require('nanoid');
+const URL = require('../models/url')
+
+async function generateShortURL(req,res){
+    const body = req.body;
+    if(!body.url) return res.status(400).json( { error : 'URL is required'})
+
+    const shortId = nanoid(8);
+
+    await URL.create({
+        shortId : shortId,
+        redirectUrl : body.url,
+        visitHistory : []
+    });
+
+    return res.json({ id : shortId})
 }
+
+module.exports = generateShortURL;
