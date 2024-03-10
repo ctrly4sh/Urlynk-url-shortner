ed3cd64cfe5e02d05b98850060c8e701030ffaa2
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-10 23:38:43 +0530

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { connectMongoDB } = require('./connect')
 const urlRoute = require('./routes/url');
+const URL = require('./models/url');
 const app = express();
 const PORT = 8080
+
 
 const connectionSuccess = connectMongoDB("mongodb://127.0.0.1:27017/urlynkDatabase")
 
@@ -13,6 +15,18 @@ connectionSuccess.then(
 app.use(express.json())
 
 app.use('/url', urlRoute)
+
+app.get('/:shortId' , async(request , response) => {
+  const shortId = request.params.shortId;
+  await URL.findOneAndUpdate(
+    {
+      shortId
+    },{push : (
+      
+    )}
+  )
+  
+})
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 })
