0febdb8fa7cca3ce01437292e6fdc2c015b98037
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-25 23:12:39 +0530

    shortUrl generated , redirected

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+
 
 const app = express();
 const PORT = 8001;
-
-
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
@@ -14,6 +15,18 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
 app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get('/:shortId' , async(req,res)=>{
+  const shortID = req.params.shortId;
+  const entry = await URL.findOneAndUpdate({
+    shortID
+  },{$push :{
+    visitHistory: {
+     timestamp :  Date.now(),
+    }
+  }})
+  res.redirect(entry.redirectURL)
+})
 
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
