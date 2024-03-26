1c5072f23c2ccf0ffe6bd0d3dbbe90140ac41e81
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-26 22:45:41 +0530

    ejs rendering

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const ejs = require("ejs"); //ejs
+const path = require("path")
 
 const app = express();
 const PORT = 8001;
@@ -12,18 +14,30 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
 
 app.use(express.json());
 
+app.set("view engine", "ejs")
+
+app.get("/test", async (req, res) => {
+  const urls = await URL.find({});
+  return res.render("home")
+});
+
 app.use("/url", urlRoute);
 
-app.get('/:shortId' , async(req,res)=>{
+app.get("/:shortId", async (req, res) => {
   const shortID = req.params.shortId;
-  const entry = await URL.findOneAndUpdate({
-    shortID
-  },{$push :{
-    visitHistory: {
-     timestamp :  Date.now(),
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortID,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
     }
-  }})
-  res.redirect(entry.redirectURL)
-})
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
